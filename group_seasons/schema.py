5aadfd7d084f702d98c4982be9b847798efd74a7
Jinho Hyeon <neverlish@gmail.com>
2024-02-21 00:44:48 +0900

    groupSeason 단건쿼리 추가

--- group_seasons/schema.py
+++ group_seasons/schema.py
@@ -1,16 +1,31 @@
-import graphene
+from graphene import ID, Field, List, NonNull, ObjectType, Schema, Union
 from graphene_django import DjangoObjectType
+
+from schema.types import NotFoundException
+
 from .models import GroupSeason
+
 
 class GroupSeasonType(DjangoObjectType):
     class Meta:
         model = GroupSeason
         name = 'GroupSeason'
 
-class Query(graphene.ObjectType):
-    groupSeasons = graphene.NonNull(graphene.List(graphene.NonNull(GroupSeasonType)))
+class GroupSeasonDetail(Union):
+    class Meta:
+        types = (GroupSeasonType, NotFoundException)
+
+class Query(ObjectType):
+    groupSeasons = NonNull(List(NonNull(GroupSeasonType)))
+    groupSeason = Field(NonNull(GroupSeasonDetail), id=NonNull(ID))
 
     def resolve_groupSeasons(self, info, **kwargs):
         return GroupSeason.objects.all()
     
-group_season_schema = graphene.Schema(query=Query)
+    def resolve_groupSeason(self, info, id):
+        groupSeason = GroupSeason.objects.filter(id=id).first()
+        if groupSeason is None:
+            return NotFoundException(message=f'GroupSeason with id {id} does not exist')
+        return groupSeason
+    
+group_season_schema = Schema(query=Query)
